23055f32371ed32ce065856044a9db7d16257bcb
Laura Nunes <lauracostanunes2@gmail.com>
2026-08-19 20:39:08 -0300

    Aula 5 - 19/08/26 - Merge sort

---
+++ 010 - Merge-sort.mjs
@@ -0,0 +1,28 @@
+function mergeSort (vetor) {
+    if (vetor.length < 2) return vetor
+
+    let meio = Math.floor(vetor.length/2)
+
+    let vetEsq = vetor.slice(0, meio) // Exclui a posição, ela não entra
+    let vetDir = vetor.slice(meio) // Conta o meio até o final
+
+    // Chamadas recursivas ao mergeSort
+    vetEsq = mergeSort(vetEsq)
+    vetDir = mergeSort(vetDir)
+
+    // Mesclagem ordenada do vetEsq com vetDir
+    let posEsq = 0, posDir = 0, vetRes = []
+
+    while(posEsq < vetEsq.length && posDir < vetDir.length) {
+        if (vetEsq[posEsq] < vetDir[posDir]) {
+            vetRes.push(vetEsq[posEsq])
+            posEsq++
+        }
+        else {
+            vetRes.push(vetDir[posDir])
+            posDir++
+        }
+    }
+}
+
+let nums = [77, 44, 22, 33, 99, 55, 88, 66, 0, 11]
